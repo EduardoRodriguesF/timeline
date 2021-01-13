2cb75c9a63b7e3179402d284c9fe36781867994f
Eduardo Rodrigues <eduaadorfernandes@gmail.com>
2021-01-13 10:26:56 -0300

    feat: fadeIn

--- src/page/App.tsx
+++ src/page/App.tsx
@@ -1,25 +1,44 @@
 import React from 'react';
+import { Fade } from "react-awesome-reveal";
 
 import { Container, Timeline } from './styles';
 
-function App() {
+const App: React.FC = () => {
   return (
     <Container>
       <Timeline>
-        <ul>
-          <li>
-            <div>Teste 1</div>
-          </li>
-          <li>
-            <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus fermentum egestas diam, at dapibus lectus lacinia at. Etiam hendrerit posuere augue nec pretium. Nam finibus lacus eget ex vulputate sagittis. Proin a libero eget turpis eleifend tincidunt id et quam. Nunc interdum scelerisque ipsum sit amet aliquam. Phasellus fermentum nisi quis quam laoreet, eu ullamcorper sem venenatis. Nam aliquet lectus sed nulla tincidunt, id ornare neque blandit. Nulla facilisi. Curabitur varius volutpat leo, egestas volutpat mi auctor varius. Morbi suscipit ante sed enim eleifend accumsan. Donec eu orci et mi finibus faucibus at vel est.</div>
-          </li>
-          <li>
-            <div>Teste 3</div>
-          </li>
-          <li>
-            <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus fermentum egestas</div>
-          </li>
-        </ul>
+          <ul>
+            <li>
+              <Fade direction="down">Teste 1</Fade>
+            </li>
+            <li>
+              <Fade direction="down">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus fermentum egestas diam, at dapibus lectus lacinia at. Etiam hendrerit posuere augue nec pretium. Nam finibus lacus eget ex vulputate sagittis. Proin a libero eget turpis eleifend tincidunt id et quam. Nunc interdum scelerisque ipsum sit amet aliquam. Phasellus fermentum nisi quis quam laoreet, eu ullamcorper sem venenatis. Nam aliquet lectus sed nulla tincidunt, id ornare neque blandit. Nulla facilisi. Curabitur varius volutpat leo, egestas volutpat mi auctor varius. Morbi suscipit ante sed enim eleifend accumsan. Donec eu orci et mi finibus faucibus at vel est.</Fade>
+            </li>
+            <li>
+              <Fade direction="down">Teste 3</Fade>
+            </li>
+            <li>
+              <Fade direction="down">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus fermentum egestas</Fade>
+            </li>
+            <li>
+              <Fade direction="down">In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.</Fade>
+            </li>
+            <li>
+              <Fade direction="down">In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.</Fade>
+            </li>
+            <li>
+              <Fade direction="down">In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.</Fade>
+            </li>
+            <li>
+              <Fade direction="down">In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.</Fade>
+            </li>
+            <li>
+              <Fade direction="down">In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.</Fade>
+            </li>
+            <li>
+              <Fade direction="down">In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.</Fade>
+            </li>
+          </ul>
       </Timeline>
     </Container>
   );
