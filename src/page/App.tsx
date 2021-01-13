c2af5c0bcbe35e80fc8d405ce1309a28dcc2ec4e
Eduardo Rodrigues <eduaadorfernandes@gmail.com>
2021-01-13 10:47:19 -0300

    chore: fadeIn effect back to direction down

--- src/page/App.tsx
+++ src/page/App.tsx
@@ -9,34 +9,34 @@ const App: React.FC = () => {
       <Timeline>
           <ul>
             <li>
-              <Fade triggerOnce direction="right">Teste 1</Fade>
+              <Fade triggerOnce direction="down">Teste 1</Fade>
             </li>
             <li>
-              <Fade triggerOnce direction="left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus fermentum egestas diam, at dapibus lectus lacinia at. Etiam hendrerit posuere augue nec pretium. Nam finibus lacus eget ex vulputate sagittis. Proin a libero eget turpis eleifend tincidunt id et quam. Nunc interdum scelerisque ipsum sit amet aliquam. Phasellus fermentum nisi quis quam laoreet, eu ullamcorper sem venenatis. Nam aliquet lectus sed nulla tincidunt, id ornare neque blandit. Nulla facilisi. Curabitur varius volutpat leo, egestas volutpat mi auctor varius. Morbi suscipit ante sed enim eleifend accumsan. Donec eu orci et mi finibus faucibus at vel est.</Fade>
+              <Fade triggerOnce direction="down">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus fermentum egestas diam, at dapibus lectus lacinia at. Etiam hendrerit posuere augue nec pretium. Nam finibus lacus eget ex vulputate sagittis. Proin a libero eget turpis eleifend tincidunt id et quam. Nunc interdum scelerisque ipsum sit amet aliquam. Phasellus fermentum nisi quis quam laoreet, eu ullamcorper sem venenatis. Nam aliquet lectus sed nulla tincidunt, id ornare neque blandit. Nulla facilisi. Curabitur varius volutpat leo, egestas volutpat mi auctor varius. Morbi suscipit ante sed enim eleifend accumsan. Donec eu orci et mi finibus faucibus at vel est.</Fade>
             </li>
             <li>
-              <Fade triggerOnce direction="right">Teste 3</Fade>
+              <Fade triggerOnce direction="down">Teste 3</Fade>
             </li>
             <li>
-              <Fade triggerOnce direction="left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus fermentum egestas</Fade>
+              <Fade triggerOnce direction="down">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus fermentum egestas</Fade>
             </li>
             <li>
-              <Fade triggerOnce direction="right">In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.</Fade>
+              <Fade triggerOnce direction="down">In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.</Fade>
             </li>
             <li>
-              <Fade triggerOnce direction="left">In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.</Fade>
+              <Fade triggerOnce direction="down">In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.</Fade>
             </li>
             <li>
-              <Fade triggerOnce direction="right">In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.</Fade>
+              <Fade triggerOnce direction="down">In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.</Fade>
             </li>
             <li>
-              <Fade triggerOnce direction="left">In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.</Fade>
+              <Fade triggerOnce direction="down">In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.</Fade>
             </li>
             <li>
-              <Fade triggerOnce direction="right">In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.</Fade>
+              <Fade triggerOnce direction="down">In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.</Fade>
             </li>
             <li>
-              <Fade triggerOnce direction="left">In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.</Fade>
+              <Fade triggerOnce direction="down">In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.</Fade>
             </li>
           </ul>
       </Timeline>
