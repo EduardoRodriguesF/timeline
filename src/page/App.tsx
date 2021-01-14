0b50ca7d2b84c9a5cca9f5695ed541288df39a06
Eduardo Rodrigues <eduaadorfernandes@gmail.com>
2021-01-14 09:33:16 -0300

    chore: passages extract from json file

--- src/page/App.tsx
+++ src/page/App.tsx
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import TimePassage from '../components/TimePassage';
 
+import data from './passages.json';
+
 import { Container, Timeline } from './styles';
 
+interface IPassage {
+  title: string;
+  date: string;
+  text: string;
+}
+
 const App: React.FC = () => {
+  const [passages, setPassages] = useState<IPassage[]>();
+
+  useEffect(() => {
+    setPassages(data);
+  }, []);
+
   return (
     <Container>
       <Timeline>
           <ul>
-            <TimePassage>
-              teste
-            </TimePassage>
-            <TimePassage>
-              teste
-            </TimePassage>
+            {passages && passages.map(passage => <TimePassage>{passage.text}</TimePassage>)}
           </ul>
       </Timeline>
     </Container>
